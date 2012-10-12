2a2cd0c6b108c0931c9a91fafb34e3f6264127ea
Chris Yuen <chris@kizzx2.com>
2012-10-12 20:48:04 +0800

    Basic swapping 1 photo version
    Don't even need an HTTP server

--- server/application.js
+++ server/application.js
@@ -1,8 +1,53 @@
-$(function()
+(function()
 {
-  var faye = new Faye.Client("http://localhost:9292/faye");
-  faye.subscribe('/punch_me', function(message)
+  function Victim($selector)
   {
-    $('.trace').append(message + "<br />");
+    this.$selector = $selector;
+  }
+
+  Victim.prototype =
+  {
+    punch: function()
+    {
+      var $face = this.$selector.find('.face');
+      $face.attr('src', 'two.jpg');
+      setTimeout(function() { $face.attr('src', 'one.jpg'); }, 200);
+    },
+
+    restore: function()
+    {
+      this.$selector.find('.face').attr('src', 'one.jpg');
+    }
+  };
+
+
+  function logTrace(x)
+  {
+    $('.trace').append(x + "<br />");
+  }
+
+  $(function()
+  {
+    var victim1 = new Victim($('.victim'));
+    var faye = new Faye.Client("http://localhost:9292/faye");
+
+    $('button.punch').on('click', function()
+    {
+      victim1.punch();
+    });
+
+    faye.subscribe('/punch_me', function(message)
+    {
+      switch(message)
+      {
+        case 'PUNCH':
+          victim1.punch();
+          break;
+
+        default:
+          logTrace("Unknown message: " + message);
+          break;
+      }
+    });
   });
-});
+})();
