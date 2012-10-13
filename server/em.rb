9e7940038325993568e749d2a2449c6146d55399
Chris Yuen <chris@kizzx2.com>
2012-10-13 15:54:00 +0800

    Receive image uploads

--- server/em.rb
+++
@@ -1,8 +0,0 @@
-require 'eventmachine'
-require './punch_me_server'
-
-PORT = 9999
-
-EventMachine.run do
-  EventMachine.start_server('0.0.0.0', PORT, PunchMeServer)
-end
--- server/punch_me_server.rb
+++ server/punch_me_server.rb
@@ -1,16 +1,48 @@
 require 'faye'
+require 'pathname'
+require 'securerandom'
+require 'debugger'
 
-FAYE_URL = 'http://localhost:9292/faye'
-FAYE_CHANNEL = '/punch_me'
+FAYE_URL = 'http://localhost:9292/faye'.freeze
+FAYE_CHANNEL = '/punch_me'.freeze
+
+IMAGE_DIR = Pathname.new(__FILE__).dirname.join(
+  'public', 'images', 'uploads').freeze
 
 module PunchMeServer
+  def post_init
+    @image_buffer = nil
+  end
+
   def receive_data(data)
-    data.lines.each do |line|
-      faye.publish(FAYE_CHANNEL, line.strip)
+    if @image_buffer
+      @image_buffer << data
+    elsif data.start_with?("PUNCH")
+      publish(line0.strip)
+      data.lines.each do |line|
+        publish(line.strip)
+      end
+    elsif data.start_with?("IMAGE")
+      @image_buffer = ""
+      @image_buffer << data[data.index("\n") + 1, data.length]
+    end
+  end
+
+  def unbind
+    if @image_buffer
+      fn = SecureRandom.uuid
+      File.write(IMAGE_DIR.join(fn), @image_buffer)
+      publish("IMAGE #{fn}")
+
+      puts "Saved file #{fn}"
     end
   end
 
   private
+
+  def publish(*args)
+    faye.publish(FAYE_CHANNEL, *args)
+  end
 
   def faye
     @faye ||= Faye::Client.new(FAYE_URL)
